fb72950d99f0b0f2b4a4d30a0d7c86afe283df25
IGOR_SOUSA_MELO <igormelo@uni9.edu.br>
2024-10-30 16:50:50 +0000

    App De Previsao

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,199 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main() => runApp(MyApp());
+class Previsao {
+  final String data;
+  final double temperatura;
+  final double umidade;
+  final double luminosidade;
+  final double vento;
+  final double chuva;
+  final String unidade;
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+  Previsao({
+    required this.data,
+    required this.temperatura,
+    required this.umidade,
+    required this.luminosidade,
+    required this.vento,
+    required this.chuva,
+    required this.unidade,
+  });
+
+  factory Previsao.fromJson(Map<String, dynamic> json) {
+    return Previsao(
+      data: json['data'],
+      temperatura: json['temperatura'],
+      umidade: json['umidade'],
+      luminosidade: json['luminosidade'],
+      vento: json['vento'],
+      chuva: json['chuva'],
+      unidade: json['unidade'],
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+void main() {
+  runApp(PrevisaoApp());
+}
+
+class PrevisaoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Previsão do Tempo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: PrevisaoPage(),
+    );
+  }
+}
+
+class PrevisaoPage extends StatefulWidget {
+  @override
+  _PrevisaoPageState createState() => _PrevisaoPageState();
+}
+
+class _PrevisaoPageState extends State<PrevisaoPage> {
+  late Future<List<Previsao>> previsoes;
+
+  @override
+  void initState() {
+    super.initState();
+    previsoes = fetchPrevisao();
+  }
+
+  Future<List<Previsao>> fetchPrevisao() async {
+    final response =
+        await http.get(Uri.parse('https://demo3520525.mockable.io/previsao'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
+      return data.map((item) => Previsao.fromJson(item)).toList();
+    } else {
+      throw Exception('Falha ao carregar a previsão do tempo');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text('Previsão do Tempo'),
+        backgroundColor: Colors.blue[800],
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      body: FutureBuilder<List<Previsao>>(
+        future: previsoes,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Erro: ${snapshot.error}'));
+          } else if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final previsao = snapshot.data![index];
+                return Card(
+                  margin: EdgeInsets.all(10),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  elevation: 5,
+                  child: Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Data: ${previsao.data}',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue[800],
+                          ),
+                        ),
+                        Divider(color: Colors.blue[200]),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            InfoTile(
+                              icon: Icons.thermostat,
+                              label: 'Temp.',
+                              value:
+                                  '${previsao.temperatura}°${previsao.unidade}',
+                            ),
+                            InfoTile(
+                              icon: Icons.water_drop,
+                              label: 'Umidade',
+                              value: '${previsao.umidade}%',
+                            ),
+                            InfoTile(
+                              icon: Icons.light_mode,
+                              label: 'Lum.',
+                              value: '${previsao.luminosidade} lux',
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 8),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            InfoTile(
+                              icon: Icons.air,
+                              label: 'Vento',
+                              value: '${previsao.vento} m/s',
+                            ),
+                            InfoTile(
+                              icon: Icons.beach_access,
+                              label: 'Chuva',
+                              value: '${previsao.chuva} mm',
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          } else {
+            return Center(child: Text('Nenhuma previsão disponível.'));
+          }
+        },
+      ),
+    );
+  }
+}
+
+class InfoTile extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final String value;
+
+  InfoTile({required this.icon, required this.label, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(icon, color: Colors.blue[600], size: 28),
+        SizedBox(height: 4),
+        Text(
+          label,
+          style: TextStyle(fontSize: 12, color: Colors.blue[800]),
         ),
-      ),
+        Text(
+          value,
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+            color: Colors.black87,
+          ),
+        ),
+      ],
     );
   }
 }
